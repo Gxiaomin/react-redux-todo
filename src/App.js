58e12cb1468d2b4ccc1213caf071444b2dc86d86
huangxiaomin <huangxiaomin@parkingwang.com>
2020-02-29 13:12:07 +0800

    实现基本功能

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+/*
+ * @Author: xiaomin
+ * @Date: 2020-02-28 19:07:56
+ * @LastEditTime: 2020-02-29 13:11:19
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/stylesheet/app.scss';
+import Todo from './views/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>React-redux Todolist</header>
+      <Todo></Todo>
     </div>
   );
 }
